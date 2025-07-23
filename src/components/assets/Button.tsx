8998d4c83dd41b8f1152de0cf94ba38e43210c42
kristofkruller <kristof.kruller@gmail.com>
2025-07-23 04:40:52 +0200

    btn color hotfix

--- src/components/assets/Button.tsx
+++ src/components/assets/Button.tsx
@@ -2,12 +2,12 @@ const ActionBtn = ({ content, onClick, disabled }: BtnProp) => {
   const colors = {
     bool:
       disabled === true
-      ? "bg-gray-400" 
+      ? "bg-gray-400 opacity-70" 
       : content.toLowerCase().startsWith("bej")
-      ? "bg-gray-700 dark:bg-white-200/60 hover:bg-gray-500/80 dark:hover:bg-gray-100/80 text-lg text-gray-300 hover:text-white dark:hover:text-indigo-500"
+      ? "bg-gray-700 dark:bg-white-200/60 hover:bg-gray-500/80 dark:hover:bg-gray-100/80 text-lg hover:text-white dark:hover:text-indigo-500"
       : content.toLowerCase().startsWith("reg")
-      ? "bg-indigo-800/20 hover:bg-indigo-400 hover:text-white text-gray-300 hover:border-indigo-300"
-      : "bg-gray-800 hover:bg-gray-700 hover:text-white text-gray-300",
+      ? "bg-indigo-800/70 hover:bg-indigo-400 hover:border-indigo-300"
+      : "bg-gray-400 hover:bg-gray-300",
   };
 
   return (
@@ -17,7 +17,7 @@ const ActionBtn = ({ content, onClick, disabled }: BtnProp) => {
       disabled={disabled}
       className={
         `actionBtn ${!disabled ? "hover:actionAnimOp cursor-pointer border-2" : ""} !px-5 !py-3 relative block focus:outline-none rounded-xl w-35 lg:w-40
-        lg:text-sm text-xs text-center font-semibold uppercase tracking-widest font-bold
+        lg:text-sm text-xs text-center text-white font-semibold uppercase tracking-widest font-bold
         ${colors.bool}`}
     >
       {content}
